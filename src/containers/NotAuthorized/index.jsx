1dc1f7198e0e14b545fafb86838fc99655250657
Victor Casas <victor.casas@outlook.cl>
2020-05-23 18:47:29 -0400

    feat: add component NotAuthorized

---
+++ src/containers/NotAuthorized/index.jsx
@@ -0,0 +1,11 @@
+/**
+ * Not Authorized
+ *
+ * Not Authorized component. Error 401
+ */
+
+import React from 'react'
+
+export default function NotAuthorized() {
+  return <div>Not Authorized</div>
+}
