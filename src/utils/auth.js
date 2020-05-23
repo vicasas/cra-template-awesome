14b8942ae35354c61a2f672f4f1841d4609a9a57
Victor Casas <victor.casas@outlook.cl>
2020-05-23 18:44:18 -0400

    fix: delete file auth.js

--- src/utils/auth.js
+++
@@ -1,9 +0,0 @@
-/**
- * Verifica si un usuario esta autenticado correctamente en la aplicación.
- *
- * @return {Booleano}
- */
-export default function isLogin() {
-  // lógica para saber si un usuario esta autenticado.
-  return false
-}
